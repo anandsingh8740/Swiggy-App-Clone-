40348827e05a697daf6011d45f3bfc205b802674
Anand Singh <140942197+anandsingh8740@users.noreply.github.com>
2024-04-01 23:17:57 +0530

    first commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -35,7 +35,6 @@ useEffect(() => {   // pass an argument
 
 const fetchData = async() => {
   const data = await fetch(
-   //"https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9319223&lng=77.696844&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 );
 
